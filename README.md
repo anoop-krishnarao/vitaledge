b9d4335523bb9bb90b1509360f64bee8ca9ff463
Anoop K <krishnarao.anoop@gmail.com>
2026-04-29 10:24:56 +0900

    scaffold: Fabric 1.21.1 client mod with Fabric API and Mod Menu

---
+++ README.md
@@ -0,0 +1,15 @@
+# VitalEdge
+
+A Minecraft Fabric client-side mod for 1.21.x.
+
+## Building
+
+```bash
+./gradlew build
+```
+
+## Development
+
+- Java 21 (Temurin recommended via SDKMAN)
+- IntelliJ IDEA
+- Fabric Loom 1.14
---
+++ src/main/java/dev/anoopkrishnarao/vitaledge/VitalEdgeClient.java
@@ -0,0 +1,16 @@
+package dev.anoopkrishnarao.vitaledge;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VitalEdgeClient implements ClientModInitializer {
+
+    public static final String MOD_ID = "vitaledge";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("VitalEdge initialised.");
+    }
+}
